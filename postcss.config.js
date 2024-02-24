88380ada2bdad241d6b5c6768102476f97daf7ab
Jherson Martelo Medina <37153701+G0m1namm@users.noreply.github.com>
2024-02-24 16:43:22 -0500

    chore: include local font face. Add fonts configuration in Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    '@csstools/postcss-oklab-function': { preserve: true }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  darkMode: 'media',
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sequel-wide': [
+          'var(--font-sequel-wide),ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+        ]
+      },
+      colors: {
+        accent: 'oklch(var(--color-accent) / <alpha-value>)',
+        primary: 'oklch(var(--color-primary) / <alpha-value>)',
+        dark: 'oklch(var(--color-dark) / <alpha-value>)',
+        light: 'oklch(var(--color-light) / <alpha-value>)'
+      }
+    }
   },
-  plugins: []
+  plugins: [require('tailwind-fluid-typography')]
 }
